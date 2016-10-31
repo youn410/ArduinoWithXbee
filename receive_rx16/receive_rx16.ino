21aa1cd519c2db48792d71b3af8654a8b91b875c
youn410 <youn410@gmail.com>
2016-10-31 20:50:08 +0900

    change recieve

--- receive_rx16/receive_rx16.ino
+++ receive_rx16/receive_rx16.ino
@@ -1,19 +1,24 @@
+#include <Printers.h>
 #include <XBee.h>
+#include <Wire.h>
 
 XBee xbee = XBee();
 Rx16Response rx16 = Rx16Response();
 
 void setup() {
-  xbee.begin(9600);
   Serial.begin(9600);
+  xbee.setSerial(Serial);
 }
 
 void loop() {
   xbee.readPacket(100);
-  if(xbee.getResponse().isAvaiable()){
+  if(xbee.getResponse().isAvailable()){
     if(xbee.getResponse().getApiId() == RX_16_RESPONSE){
       xbee.getResponse().getRx16Response(rx16);
-      Serial.print(rx16.getRssi());
+      Serial.print("RSSI : ");
+      Serial.println(rx16.getRssi());
     }
+  }else{
+    Serial.println(xbee.getResponse().isAvailable());
   }
 }
