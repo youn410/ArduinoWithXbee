bd6dc4c0cfc116cba551b781780c54e47ab4b1e8
youn410 <youn410@gmail.com>
2016-10-31 20:27:27 +0900

    update send

--- send_rx16/send_rx16.ino
+++ send_rx16/send_rx16.ino
@@ -2,14 +2,30 @@
 
 XBee xbee = XBee();
 uint8_t payload[] = {'H', 'i'};
-XBeeAddress64 addr64 = XBeeAddress64(,);
-Tx16Request tx16 = Tx16Request(addr64, payload, sizeof(payload));
+XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x40B3C07A);
+Tx64Request tx64 = Tx64Request(addr64, payload, sizeof(payload));
 
 void setup() {
-  xbee.begin(9600);
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
 }
 
 void loop() {
-  xbee.send(tx16);
-  delay(50);
+  xbee.send(tx64);
+  delay(1000);
+  Serial.print("Send packet!! --> ");
+  
+  xbee.readPacket(100);
+  if(xbee.getResponse().isAvailable()){
+    if(xbee.getResponse().getApiId() == TX_STATUS_RESPONSE){
+      Serial.print("TX_STATUS_RESPONSE : ");
+      Serial.println(xbee.getResponse().getApiId());
+    }else{
+      Serial.print("not TX_STATUS_RESPONSE : ");
+      Serial.println(xbee.getResponse().getApiId());
+    }
+  }else{
+    Serial.println("Miss!!");
+  }
+  delay(4000);
 }
