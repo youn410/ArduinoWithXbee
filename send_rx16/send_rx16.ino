e3799d458332da17915dabe91f0fc6e76e0e5fb6
youn410 <youn410@gmail.com>
2016-11-02 14:01:00 +0900

    電波強度の取得

--- send_rx16/send_rx16.ino
+++ send_rx16/send_rx16.ino
@@ -3,9 +3,11 @@
 
 XBee xbee = XBee();
 uint8_t payload[] = { 0 , 0 };
-XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x40B3C215);
-//XBeeAddress64 addr64 = XBeeAddress64(0xFFFFFFFF, 0xFFFFFFFF);
-Tx64Request tx64 = Tx64Request(addr64, payload, sizeof(payload));
+//XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x40B3C215);
+XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x0000ffff);
+//Tx64Request tx64 = Tx64Request(addr64, payload, sizeof(payload));
+uint8_t cmd[] = {'D', 'B'};
+RemoteAtCommandRequest remoteAtRequest = RemoteAtCommandRequest(addr64, cmd);
 
 void setup() {
   Serial.begin(9600);
@@ -13,17 +15,25 @@ void setup() {
 }
 
 void loop() {
-  xbee.send(tx64);
+  xbee.send(remoteAtRequest);
   delay(1000);
   Serial.print("Send packet!! --> ");
   
   xbee.readPacket(500);
   if(xbee.getResponse().isAvailable()){
-    if(xbee.getResponse().getApiId() == TX_STATUS_RESPONSE){
-      Serial.print("TX_STATUS_RESPONSE : ");
-      Serial.println(xbee.getResponse().getApiId());
+    if(xbee.getResponse().getApiId() == REMOTE_AT_COMMAND_RESPONSE){
+      //Serial.print("REMOTE_AT_COMMAND_RESPONSE : ");
+      //Serial.println(xbee.getResponse().getApiId());
+      Serial.print("Value : ");
+      uint8_t frameDataLength = xbee.getResponse().getFrameDataLength();
+      uint8_t* frameData = xbee.getResponse().getFrameData();
+      for(int i=0;i<frameDataLength;i++){
+        Serial.print(frameData[i]);
+        Serial.print(" ");
+      }
+      Serial.println("");
     }else{
-      Serial.print("not TX_STATUS_RESPONSE : ");
+      Serial.print("not REMOTE_AT_COMMAND_RESPONSE : ");
       Serial.println(xbee.getResponse().getApiId());
     }
   }else{
