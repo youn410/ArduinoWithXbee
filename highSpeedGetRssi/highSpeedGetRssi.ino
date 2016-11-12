d4dbbcc36b2e9b13b21b4534dc57ea4ec0223077
youn410 <youn410@gmail.com>
2016-11-12 11:21:26 +0900

    変更

--- highSpeedGetRssi/highSpeedGetRssi.ino
+++ highSpeedGetRssi/highSpeedGetRssi.ino
@@ -6,14 +6,18 @@ uint8_t payload[] = { 0 , 0 };
 //XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40b3c215);
 XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x0000ffff);
 uint8_t cmd[] = {'D', 'B'};
+uint8_t cmdVal[] = {0};
 RemoteAtCommandRequest remoteAtRequest = RemoteAtCommandRequest(addr64, cmd);
-//RemoteAtCommandResponse remoteResp = RemoteAtCommandResponse();
+RemoteAtCommandResponse remoteResp = RemoteAtCommandResponse();
 
-int is_available[100];
-uint8_t api_id[100];
-uint8_t r_status[100];
-uint8_t value[100];
-int count = 0;
+/*
+void getRemoteAtReqFrame(){
+  Serial.print("RemoteAtCommand : ");
+  Serial.print(remoteAtRequest.getFrameData(11), HEX);
+  Serial.print(remoteAtRequest.getFrameData(12), HEX);
+  Serial.print("||");
+}
+*/
 
 void setup() {
   Serial.begin(9600);
@@ -22,15 +26,11 @@ void setup() {
 }
 
 void loop() {
-  if(count < 10000){
-    xbee = XBee();
+
+    //getRemoteAtReqFrame();
+
     xbee.send(remoteAtRequest);
     xbee.readPacket(1500);
-    
-    //is_available[count] = xbee.getResponse().isAvailable();
-    //api_id[count] = xbee.getResponse().getApiId();
-    
-    RemoteAtCommandResponse remoteResp = RemoteAtCommandResponse();
     
     xbee.getResponse().getRemoteAtCommandResponse(remoteResp);
   
@@ -39,25 +39,8 @@ void loop() {
     Serial.print(' ');
     Serial.println(remoteResp.getValue()[0]);
 
-    //r_status[count] = remoteResp.getStatus();
-    //value[count] = remoteResp.getValue()[0];
-
-    count++;
+    remoteResp.getFrameData();
     
     delay(1500);
-  /*}else if(count == 100){
-    Serial.println("");
-      for(int i=0;i<100;i++){
-        Serial.print(api_id[i], HEX);
-        Serial.print(", ");
-        Serial.println(value[i]);
-      }
-      
-      count = 0;
-      
-      delay(1000);
-      */
-  }
-  
 
 }
